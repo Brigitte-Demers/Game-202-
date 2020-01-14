0e5d5ccae447a47e9cae43a380e979a6e62df412
Brigitte Demers [Student] <brigitte.demers@student.sl.on.ca>
2020-01-14 10:59:08 -0500

    Completed Part Four. Got a game of Pong!

--- LAB 1-PONG/Pong/Pong/main.cpp
+++ LAB 1-PONG/Pong/Pong/main.cpp
@@ -40,6 +40,51 @@ SDL_Rect Ball;
 */
 SDL_Rect Middle;
 
+bool check_collision(SDL_Rect A, SDL_Rect B)
+{
+	// The sides of the rectangles.
+	int leftA, leftB;
+	int rightA, rightB;
+	int topA, topB;
+	int bottomA, bottomB;
+
+	// Calculate the sides of rectangle A.
+	leftA = A.x;
+	rightA = A.x + A.w;
+	topA = A.y;
+	bottomA = A.y + A.h;
+
+	// Calculate the sides of rectangle B.
+	leftB = B.x;
+	rightB = B.x + B.w;
+	topB = B.y;
+	bottomB = B.y + B.h;
+
+	// If any of the sides from A are outside of B.
+	if (bottomA <= topB)
+	{
+		return false;
+	}
+
+	if (topA >= bottomB)
+	{
+		return false;
+	}
+
+	if (rightA <= leftB)
+	{
+		return false;
+	}
+
+	if (leftA >= rightB)
+	{
+		return false;
+	}
+
+	// If none of the sides from A are outside of B.
+	return true;
+}
+
 /*
 Purpose of the LoadGamer function:
 */
@@ -149,6 +194,11 @@ void Update()
 	}
 
 	AIPaddle.y = Ball.y - AIPaddle.h / 2 + Ball.h / 2;
+
+	if (check_collision(Ball, AIPaddle) || check_collision(Ball, PlayerPaddle))
+	{
+		speed_x = -speed_x;
+	}
 }
 
 /*
